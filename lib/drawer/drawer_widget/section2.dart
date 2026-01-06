b5920329afdea238ea1c4453d06b402170eb0b2d
VisheshBansal1 <169073397+VisheshBansal1@users.noreply.github.com>
2026-01-06 15:49:31 +0530

    all app after ai and notes

--- lib/drawer/drawer_widget/section2.dart
+++ lib/drawer/drawer_widget/section2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:learnify/ai/choose_carier/screen/tech_comparison_screen.dart';
 import 'package:learnify/ai/roadmap_generator/screens/roadmap_generator_screen.dart';
+
 import 'package:learnify/notes/notes_screen/notes_main_screen.dart';
 
 class Section2 extends StatelessWidget {
